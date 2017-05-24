3284882bbe2e50ca5eda9131e07ac90de279dea4
Ryo Okazaki <ryo.okazaki.0929@gmail.com>
2017-05-24 15:59:40 +0900

    Try to fix a bug

--- test/travis_test_buzzer.py
+++ test/travis_test_buzzer.py
@@ -48,7 +48,7 @@ class BuzzerTest(unittest.TestCase):
     def feedback_cb(self, feedback):
         with open("/dev/rtbuzzer0", "r") as f:
             data = f.readline()
-            self.device_values.append(int(rstrip()))
+            self.device_values.append(int(data.rstrip()))
 
 if __name__ == '__main__':
     time.sleep(3)
